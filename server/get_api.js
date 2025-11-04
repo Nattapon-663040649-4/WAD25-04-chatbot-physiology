25672d6db6813d6b422884a8fef7c30cfad4a689
Nattapon <nattapon.jo@kkumail.com>
2025-11-05 04:00:10 +0700

    Web_api_url

--- server/get_api.js
+++ server/get_api.js
@@ -10,6 +10,8 @@ const bcrypt = require("bcryptjs");
 const app = express();
 const uploads = multer({ dest: "uploads/" });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // ==================== CORS Configuration ====================
 const cors = require('cors');
 
